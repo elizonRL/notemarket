ab88fe7be06c6d3bd07eae0a4bfdd05f3695fd6b
Elizon Rodrigues Liriano <118481749+elizonRL@users.noreply.github.com>
2025-10-24 11:19:09 -0400

    Feat: implement ProductContext and ProductProvider for product state management

---
+++ src/contex/productContex.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from 'react'
+
+export const ProductContext = createContext()
+
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([])
+  return (
+    <ProductContext.Provider value={{ products, setProducts }}>
+      {children}
+    </ProductContext.Provider>
+  )
+}
+
+export const useProductContext = () => {
+  return useContext(ProductContext)
+}
